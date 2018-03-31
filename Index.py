e45b7802e3e795757a69666dbabeb86b12303566
RajeshKalla <rajeshkalla308@gmail.com>
2018-03-31 19:40:51 +0530

    file adding test

--- Index.py
+++ Index.py
@@ -1,8 +1,4 @@
 # TrialProject
 print("Hi everyone!!!")
-<<<<<<< HEAD
 print("How are you")
 print("Hi everyone, this is rajesh!")
-=======
-print("How are you?")
->>>>>>> 7861645881fe8a1dad3114f139a5e181e26c87df
---
+++ Test.py
@@ -0,0 +1,2 @@
+# Test program to add file to git
+print("I added a file in to this!")
